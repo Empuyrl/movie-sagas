abcdfffaebcd416473002a5000b7e850cfdd3cbb
User <user@Users-MBP.hitronhub.home>
2023-06-11 16:59:45 -0500

    imported everything to app through routes/links

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,40 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import './App.css';
+import MovieList from '../MovieList/MovieList';
+import Details from '../Details/Details';
+import AddMovie from '../AddMovie/AddMovie';
+// Material UI
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>        
+        <header>
+            <img className="header-img" src={process.env.PUBLIC_URL + '/images/movies.jpg'} />
+        </header>
+      <Router> 
+        <nav>
+          <Link className="link" to="/" exact>Movie List</Link>
+          <Link className="link" to="/addMovie" exact>Add Movie</Link>
+        </nav>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
         {/* Details page */}
-
+        <Route path="/details" exact>
+          <Details />
+        </Route>
         {/* Add Movie page */}
+        <Route path="/addMovie" exact>
+          <AddMovie />
+        </Route>
       </Router>
     </div>
   );
 }
-
 
 export default App;
