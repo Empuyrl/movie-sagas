7d3e468071a42684f057a2543a2efa8535791b92
User <user@Users-MBP.hitronhub.home>
2023-06-11 16:49:06 -0500

    got list into a map with material ui, imported things to multipls parts of the project

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,32 +1,48 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
+import MovieSpecs from '../MovieSpecs/MovieSpecs';
+
+// Material UI imports
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+});
 
 function MovieList() {
 
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
 
+    // load movies on page load
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
     return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
+        // Maping through the movie reducer from index to display each movie
+        <Container>
+            <Typography variant="h4" gutterBottom>Movie List</Typography>
+            <Grid container spacing={5} className="movies">
+                {movies.map(movie => (
+                    // implemented grid to make page responsive
+                    <Grid 
+                        item xs={12} md={4} lg={3} 
+                        key={movie.id} 
+                    >
+                        <MovieSpecs movie={movie}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
 
     );
 }
-
 export default MovieList;
--- src/components/MovieSpecs/MovieSpecs.jsx
+++ src/components/MovieSpecs/MovieSpecs.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
