6b232db0a2f25789f3a1ef5264064e522cc5c1e3
User <user@Users-MBP.hitronhub.home>
2023-06-12 07:32:06 -0500

    added comments to a few pages for a better resource

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -1,10 +1,17 @@
+// Import the Express module
 const express = require('express');
-const router = express.Router();
-const pool = require('../modules/pool')
 
+// Create an instance of an Express router
+const router = express.Router();
+
+// Import the 'pool' module for connecting to the database
+const pool = require('../modules/pool');
+
+// Define a route for handling GET requests to the root path ('/')
 router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+  // Placeholder: Send an HTTP status code of 500 (Internal Server Error) as the response
+  res.sendStatus(500);
 });
 
+// Export the router object
 module.exports = router;
--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -2,13 +2,17 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
+//route GETS all movies
 router.get('/', (req, res) => {
-
+//selects all the movies and orders them by title
   const query = `SELECT * FROM movies ORDER BY "title" ASC`;
+  //where the connection to the database is made
   pool.query(query)
     .then( result => {
+      //sends the movie data as a respond
       res.send(result.rows);
     })
+    //error if data isn't recieved
     .catch(err => {
       console.log('ERROR: Get all movies', err);
       res.sendStatus(500)
@@ -16,10 +20,15 @@ router.get('/', (req, res) => {
 
 });
 
+//route for getting details of a specific moive
 router.get('/:movieid', (req, res) => {
+  //gets the id of the movie based on the parameters
   let id = req.params.movieid;
   console.log('id is ', id);
-  
+  //query to select movie details and associated info
+  //array_agg takes the info in as an array, so [id, title, poster, desciption]
+  //and returns the info set as values of the element asked
+  //sql key words for making a JOIN statement throuugh junction tables
   const selectQuery = `
       SELECT
         movies.id,
@@ -46,9 +55,11 @@ router.get('/:movieid', (req, res) => {
     })
 });
 
+//route for creating a new movie
 router.post('/', (req, res) => {
   console.log(req.body);
   // RETURNING "id" will give us back the id of the created movie
+  //puts the new movie in the "movies" table
   const insertMovieQuery = `
   INSERT INTO "movies" ("title", "poster", "description")
   VALUES ($1, $2, $3)
@@ -58,7 +69,7 @@ router.post('/', (req, res) => {
   pool.query(insertMovieQuery, [req.body.title, req.body.poster, req.body.description])
   .then(result => {
     console.log('New Movie Id:', result.rows[0].id); //ID IS HERE!
-    
+    //gets the id of the newly created movie
     const createdMovieId = result.rows[0].id
 
     // Now handle the genre reference
@@ -67,6 +78,7 @@ router.post('/', (req, res) => {
       VALUES  ($1, $2);
       `
       // SECOND QUERY ADDS GENRE FOR THAT NEW MOVIE
+      //sets the genre for the new movie
       pool.query(insertMovieGenreQuery, [createdMovieId, req.body.genre_id]).then(result => {
         //Now that both are done, send back success!
         res.sendStatus(201);
